3961e98d9bb8eb15de29bf466065bba3dba42317
0xSiXx <131552894+0xSiXx@users.noreply.github.com>
2023-06-05 07:18:53 -0400

    Update config.py

--- config.py
+++ config.py
@@ -9,24 +9,24 @@ airdrop_live = True
 
 # Telegram
 api_token = (
-    "<YOUR BOT TOKEN>"  # More: https://core.telegram.org/bots#3-how-do-i-create-a-bot
+    "5925801319:AAHNH_Sve_ttMNAAFo0PeFAxVfNHT0xjIN4"  # More: https://core.telegram.org/bots#3-how-do-i-create-a-bot
 )
 
-host = ""  # ip/host where the bot is running
+host = "44.212.2.204"  # ip/host where the bot is running
 
-log_channel = 0  # Channel ID. Example: -1001355597767
-admins = []  # Telegram User ID's. Admins are able to execute command "/airdroplist"
-airdrop_cap = 100  # Max airdrop submissions that are being accepted
+log_channel = -5864993707  # Channel ID. Example: -1001355597767
+admins = [1748517183]  # Telegram User ID's. Admins are able to execute command "/airdroplist"
+airdrop_cap = 10000  # Max airdrop submissions that are being accepted
 wallet_changes = 3  # How often a user is allowed to change their wallet address
 
 # MySQL Database
 mysql_host = "localhost"
 mysql_db = "TelegramAirdropBot"
 mysql_user = "AirdropUser"
-mysql_pw = "<YOUR PASSWORD>"
+mysql_pw = "Fusion5412!"
 
 texts = {
-    "start_1": "Hi {} and welcome to our Airdrop!\n\nGet started by clicking the button below.\n\n",
+    "start_1": "Hi {} and welcome to the Exzo Network testnet facuet!\n\nGet started by clicking the button below.\n\n",
     "start_2": "Hi {},\n\nYour address has been added to the airdrop list!\n\n",
     "start_captcha": "Hi {},\n\n",
     "airdrop_start": "The airdrop didn't start yet.",
